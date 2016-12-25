2bdb5e51085fcfec7610e4db1f59202271694a8e
aymen korkot <ay.korkot@feeling-group.com>
2016-12-25 11:55:00 +0000

    modif4

--- conversion/js/scripts.js
+++ conversion/js/scripts.js
@@ -2,7 +2,11 @@
 
 document.getElementById("convertMoney").onchange = function () {
 
-
+	var devise = new Array();
+	devise["eur"] = 10.6290;
+	devise["mad"] = 1;
+	devise["usd"] = 10.1752172;
+	devise = Array()
 
     var dirham = document.getElementById("dirham").value;
     var euro = document.getElementById("euro").value;
